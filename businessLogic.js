035f7002de03df550660f0aefe51c73671706c33
Bimal Grewal <bigrewal@cisco.com>
2015-06-19 00:30:11 -0700

    notifications table working

--- businessLogic.js
+++ businessLogic.js
@@ -3,16 +3,18 @@ function showSearchBar()
 	var className = $('.navigationPanel')[0].className;
 	if(className == 'navigationPanel')
 	{
-		console.log($('.navigationPanel'));
+		//console.log($('.navigationPanel'));
 		$('.navigationPanel').addClass('expandNavigationPanel');
 
-		console.log($('#search'));
+		//console.log($('#search'));
 		$('#search')[0].style.display = 'block';
+		$('.notificationsTable')[0].style.display = 'none';
 	}
 	else
 	{
 		$('.navigationPanel').removeClass().addClass('navigationPanel');
 		$('#search')[0].style.display = 'none';
+		$('.notificationsTable')[0].style.display = 'none';
 	}
 
     return false;
@@ -21,12 +23,55 @@ function showSearchBar()
 function showEvents()
 {
 	var value = $('#search')[0].value;
-	if(value != "")
+	if(value != "" && value[0] == 'h')
 	{
 		$('.searchResults')[0].style.display = 'block';
+	}
+	else if(value != "" && value[0] == 'd')
+	{
+		$('.searchResultsSecond')[0].style.display = 'block';
 	}
 	else
 	{
 		$('.searchResults')[0].style.display = 'none';
+		$('.searchResultsSecond')[0].style.display = 'none';
 	}
+}
+
+function subscribe ()
+{
+	$('#subscribeIcon').removeClass().addClass('glyphicon glyphicon-ok');
+	$('.nameAreaTwo')[0].style.display = 'block';
+	$('.nameArea')[0].style.display = 'none';
+
+}
+
+function rightArrowClick ()
+{
+	$('.nameAreaTwo')[0].style.display = 'block';
+	$('.nameArea')[0].style.display = 'none';
+}
+function leftArrowClick ()
+{
+	$('.nameAreaTwo')[0].style.display = 'none';
+	$('.nameArea')[0].style.display = 'block';
+}
+
+function showNotifications()
+{
+	var className = $('.notificationsTable')[0].style.display;
+	if(className == 'none')
+	{
+		$('#search')[0].style.display = 'none';
+		$('.navigationPanel').addClass('expandNavigationPanel');
+		console.log($('.notificationsTable'));
+		$('.notificationsTable')[0].style.display = 'block';
+	}
+	else
+	{
+		$('.navigationPanel').removeClass().addClass('navigationPanel');
+		$('#search')[0].style.display = 'none';
+		$('.notificationsTable')[0].style.display = 'none';
+	}
+
 }
