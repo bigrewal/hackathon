5b66a967df67de4c36073d8c8e2a1675296c5ee9
M43a <Waaslam@cisco.com>
2015-06-19 01:17:36 -0700

    Navbar Functionality - Bug Description: click hub button > it expands > click Search Icon and Text is still visible

--- businessLogic.js
+++ businessLogic.js
@@ -1,19 +1,32 @@
-function showSearchBar()
-{
+function showSearchBar(){
 	var className = $('.navigationPanel')[0].className;
-	if(className == 'navigationPanel')
-	{
-		console.log($('.navigationPanel'));
+	if(className == 'navigationPanel'){
+		//console.log($('.navigationPanel'));
 		$('.navigationPanel').addClass('expandNavigationPanel');
 
 		console.log($('#search'));
 		$('#search')[0].style.display = 'block';
 	}
-	else
-	{
+	else{
 		$('.navigationPanel').removeClass().addClass('navigationPanel');
 		$('#search')[0].style.display = 'none';
 	}
-
     return false;
 }
+
+function theHub(){
+	var className = $('.navigationPanel')[0].className;
+	//console.log(className);
+	if(className == 'navigationPanel'){
+		$('.navigationPanel').addClass('expandNavigationPanel');
+		//console.log($('.hub'));
+		$('div.hub')[0].style.display = 'block';
+	}
+		else{
+			$('.navigationPanel').removeClass().addClass('navigationPanel');
+			$('#search')[0].style.display = 'none';
+			$('.hub')[0].style.display = 'none';
+		}
+	return false;
+} 
+
